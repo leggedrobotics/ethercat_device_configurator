989f6acfb437bae2b963ab86ac71b42180551606
Jonas Junger <jungerj@student.ethz.ch>
2020-09-28 18:20:52 +0200

    example documentation

--- src/standalone.cpp
+++ src/standalone.cpp
@@ -1,3 +1,24 @@
+/*
+** Simple Example executable for the use of the RSL EtherCAT software tools
+** ════════════════════════════════════════════════════════════════════════
+**
+**   To understand the logic it is best to start at the main function and
+**   then go through the code step by step. The executable compiled from
+**   this source code needs to be executed as root. Simply executing the
+**   executable with sudo will not work because the catkin workspace won’t
+**   be sourced and the linker cannot find the necessary libraries. Use the
+**   following command to start this executable:
+**   ┌────
+**   │ sudo bash -c 'source /absolute/path/to/your/catkin_ws/devel/setup.bash; path/to/executable path/to/setup.yaml'
+**   └────
+**
+**
+** Build errors
+** ────────────
+**
+**   Are you using gcc and g++ both with a version >= 8.4? See the
+**   README.md for more details.
+ */
 #include "ethercat_device_configurator/EthercatDeviceConfigurator.hpp"
 
 #include <thread>
@@ -12,15 +33,33 @@ bool abrt = false;
 EthercatDeviceConfigurator::SharedPtr configurator;
 
 unsigned int counter = 0;
+
 void worker()
 {
+    /*
+    ** The communication update loop.
+    ** This loop is supposed to be executed at a constant rate.
+    ** The EthercatMaster::update function incorporates a mechanism
+    ** to create a constant rate.
+     */
     while(!abrt)
     {
+        /*
+        ** Update each master.
+        ** This sends tha last staged commands and reads the latest readings over EtherCAT.
+        ** The StandaloneEnforceRate update mode is used.
+        ** This means that average update rate will be close to the target rate (if possible).
+         */
         for(const auto & master: configurator->getMasters() )
         {
             master->update(ecat_master::UpdateMode::StandaloneEnforceRate);
         }
 
+        /*
+        ** Do things with the attached devices.
+        ** Your lowlevel control input / measurement logic goes here.
+        ** Different logic can be implemented for each device.
+         */
         for(const auto & slave:configurator->getSlaves())
         {
             if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Anydrive)
@@ -53,24 +92,47 @@ void worker()
     }
 }
 
+/*
+** Handle the interrupt signal.
+** This is the shutdown routine.
+** Note: This logic is executed in a thread separated from the communication update!
+ */
 void signal_handler(int sig)
 {
+    /*
+    ** Pre shutdown procedure.
+    ** The devices execute procedures (e.g. state changes) that are necessary for a
+    ** proper shutdown and that must be done with PDO communication.
+    ** The communication update loop (i.e. PDO loop) continues to run!
+    ** You might thus want to implement some logic that stages zero torque / velocity commands
+    ** or simliar safety measures at this point using e.g. atomic variables and checking them
+    ** in the communication update loop.
+     */
     for(const auto & master: configurator->getMasters())
     {
         master->preShutdown();
     }
 
+    // stop the PDO communication at the next update of the communication loop
     abrt = true;
     worker_thread->join();
+
+    /*
+    ** Completely halt the EtherCAT communication.
+    ** No online communication is possible afterwards, including SDOs.
+     */
     for(const auto & master: configurator->getMasters())
     {
         master->shutdown();
     }
+
+    // Exit this executable
     std::cout << "Shutdown" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     exit(0);
 }
 
+// Some dummy callbacks
 void anydriveReadingCb(const std::string& name, const anydrive::ReadingExtended& reading)
 {
     std::cout << "Reading of anydrive '" << name << "'\n"
@@ -82,10 +144,31 @@ void rokubiReadingCb(const std::string& name, const rokubi::Reading& reading)
               << "Force X: " << reading.getForceX() << "\n\n";
 }
 
+
+
+/*
+** Program entry.
+** Pass the path to the setup.yaml file as first command line argument.
+ */
 int main(int argc, char**argv)
 {
+    // Set the abrt_ flag upon receiving an interrupt signal (e.g. Ctrl-c)
+    std::signal(SIGINT, signal_handler);
+
+    if(argc < 2)
+    {
+        std::cerr << "pass path to 'setup.yaml' as command line argument" << std::endl;
+        return EXIT_FAILURE;
+    }
+    // a new EthercatDeviceConfigurator object (path to setup.yaml as constructor argument)
     configurator = std::make_shared<EthercatDeviceConfigurator>(argv[1]);
 
+    /*
+    ** Add callbacks to the devices that support them.
+    ** If you don't want to use callbacks this part can simply be left out.
+    ** configurator->getSlavesOfType is another way of extracting only the evices
+    ** of a ceratin type.
+     */
     for(const auto& device : configurator->getSlavesOfType<anydrive::AnydriveEthercatSlave>())
     {
         device->addReadingCb(anydriveReadingCb);
@@ -94,26 +177,39 @@ int main(int argc, char**argv)
         device->addReadingCb(rokubiReadingCb);
     }
 
-    std::signal(SIGINT, signal_handler);
+    /*
+    ** Start all masters.
+    ** There is exactly one bus per master which is also started.
+    ** All online (i.e. SDO) configuration is done during this call.
+    ** The EtherCAT interface is active afterwards, all drives are in Operational
+    ** EtherCAT state and PDO communication may begin.
+     */
     for(auto & master: configurator->getMasters())
     {
-        master->startup();
+        if(!master->startup())
+        {
+            std::cerr << "Startup not successful." << std::endl;
+            return EXIT_FAILURE;
+        }
     }
 
+    // Start the PDO loop in a new thread.
     worker_thread = std::make_unique<std::thread>(&worker);
 
+    /*
+    ** Wait for a few PDO cycles to pass.
+    ** Set anydrives into to ControlOp state (internal state machine, not EtherCAT states)
+     */
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     for(auto & slave: configurator->getSlaves())
     {
         std::cout << " " << slave->getName() << ": " << slave->getAddress() << std::endl;
         if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Anydrive)
         {
-
+            // Downcasting using shared pointers
             anydrive::AnydriveEthercatSlave::SharedPtr any_slave_ptr = std::dynamic_pointer_cast<anydrive::AnydriveEthercatSlave>(slave);
-
             any_slave_ptr->setFSMGoalState(anydrive::fsm::StateEnum::ControlOp, false,0,0);
             std::cout << "Putting slave into operational mode: " << any_slave_ptr->getName() << " : " << any_slave_ptr->getAddress() << std::endl;
-
         }
     }
 
@@ -121,6 +217,6 @@ int main(int argc, char**argv)
     std::cout << "Startup finished" << std::endl;
     while(true)
     {
-
+        // Wait for termination
     }
 }
