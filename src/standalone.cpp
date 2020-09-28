74b570fc924b264f61b201d00ed69ec4339a9f80
Jonas Junger <jungerj@student.ethz.ch>
2020-09-28 13:21:11 +0200

    test callbacks

--- src/standalone.cpp
+++ src/standalone.cpp
@@ -2,8 +2,9 @@
 
 #include <thread>
 
-#include "anydrive/Anydrive.hpp"
-#include "rokubi_rsl_ethercat_sdk/Rokubi.hpp"
+#include <anydrive/Anydrive.hpp>
+#include <rokubi_rsl_ethercat_sdk/Rokubi.hpp>
+#include <elmo_ethercat_sdk/Elmo.hpp>
 #include <csignal>
 std::unique_ptr<std::thread> worker_thread;
 bool abrt = false;
@@ -37,12 +38,15 @@ void worker()
                 }
 
             }
-            if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Rokubi)
+            else if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Rokubi)
             {
                 std::shared_ptr<rokubi::Rokubi> rokubi_slave_ptr = std::dynamic_pointer_cast<rokubi::Rokubi>(slave);
-
-                if(counter%100 == 0)
-                    std::cout << rokubi_slave_ptr->getReading().getForceX() << std::endl;
+                // Do things with the Rokubi sensors here
+            }
+            else if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Elmo)
+            {
+                std::shared_ptr<elmo::Elmo> elmo_slave_ptr = std::dynamic_pointer_cast<elmo::Elmo>(slave);
+                // Do things with the Elmo drives here
             }
         }
         counter++;
@@ -67,9 +71,28 @@ void signal_handler(int sig)
     exit(0);
 }
 
+void anydriveReadingCb(const std::string& name, const anydrive::ReadingExtended& reading)
+{
+    std::cout << "Reading of anydrive '" << name << "'\n"
+              << "Joint velocity: " << reading.getState().getJointVelocity() << "\n\n";
+}
+void rokubiReadingCb(const std::string& name, const rokubi::Reading& reading)
+{
+    std::cout << "Reading of rokubi '" << name << "'\n"
+              << "Force X: " << reading.getForceX() << "\n\n";
+}
+
 int main(int argc, char**argv)
 {
     configurator = std::make_shared<EthercatDeviceConfigurator>(argv[1]);
+
+    for(const auto& device : configurator->getSlavesOfType<anydrive::AnydriveEthercatSlave>())
+    {
+        device->addReadingCb(anydriveReadingCb);
+    }
+    for(const auto& device : configurator->getSlavesOfType<rokubi::Rokubi>()){
+        device->addReadingCb(rokubiReadingCb);
+    }
 
     std::signal(SIGINT, signal_handler);
     for(auto & master: configurator->getMasters())
