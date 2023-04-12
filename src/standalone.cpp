9d563d5905ef34afc35f45f41405603dfa7231da
Jan Preisig <preisigj@ethz.ch>
2023-04-12 11:34:42 +0200

    changed example to run masters update loop in separate threads

--- src/standalone.cpp
+++ src/standalone.cpp
@@ -50,31 +50,26 @@
 #endif
 #include <thread>
 #include <csignal>
-std::unique_ptr<std::thread> worker_thread;
+// std::unique_ptr<std::thread> worker_thread;
 bool abrt = false;
+bool abrtMain = false;
 
 EthercatDeviceConfigurator::SharedPtr configurator;
 
 unsigned int counter = 0;
 
-void worker()
+void worker(int masterIndex)
 {
-    bool rtSuccess = true;
-    for(const auto & master: configurator->getMasters())
-    {
-        rtSuccess &= master->setRealtimePriority(99);
-    }
+    // bool rtSuccess = true;
+    // for(const auto & master: configurator->getMasters())
+    // {
+    //     rtSuccess &= master->setRealtimePriority(99);
+    // }
+    std::cout << "updating master with index: " << masterIndex << std::endl;
+    
+    bool rtSuccess = configurator->getMasters().at(masterIndex)->setRealtimePriority(99);
     std::cout << "Setting RT Priority: " << (rtSuccess? "successful." : "not successful. Check user privileges.") << std::endl;
 
-    // Flag to set the drive state for the elmos on first startup
-#ifdef _ELMO_FOUND_
-    bool elmoEnabledAfterStartup = false;
-#endif
-    // Flag to set the drive state for the elmos on first startup
-#ifdef _MAXON_FOUND_
-    bool maxonEnabledAfterStartup = false;
-    // bool maxonOperational = false;
-#endif
     /*
     ** The communication update loop.
     ** This loop is supposed to be executed at a constant rate.
@@ -89,115 +84,11 @@ void worker()
         ** The StandaloneEnforceRate update mode is used.
         ** This means that average update rate will be close to the target rate (if possible).
          */
-        for(const auto & master: configurator->getMasters() )
-        {
-            master->update(ecat_master::UpdateMode::StandaloneEnforceRate); // TODO fix the rate compensation (Elmo reliability problem)!!
-        }
-
-        /*
-        ** Do things with the attached devices.
-        ** Your lowlevel control input / measurement logic goes here.
-        ** Different logic can be implemented for each device.
-         */
-        for(const auto & slave:configurator->getSlaves())
-        {
-            // Anydrive
-            if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Anydrive)
-            {
-#ifdef _ANYDRIVE_FOUND_
-                anydrive::AnydriveEthercatSlave::SharedPtr any_slave_ptr = std::dynamic_pointer_cast<anydrive::AnydriveEthercatSlave>(slave);
-
-                if(any_slave_ptr->getActiveStateEnum() == anydrive::fsm::StateEnum::ControlOp)
-                {
-                    anydrive::Command cmd;
-                    cmd.setModeEnum(anydrive::mode::ModeEnum::MotorVelocity);
-                    cmd.setMotorVelocity(10);
-
-                    any_slave_ptr->setCommand(cmd);
-                }
-#endif
-
-            }
-            // Rokubi
-            else if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Rokubi)
-            {
-#ifdef _ROKUBI_FOUND_
-                std::shared_ptr<rokubimini::ethercat::RokubiminiEthercat> rokubi_slave_ptr = std::dynamic_pointer_cast<rokubimini::ethercat::RokubiminiEthercat>(slave);
-                // Do things with the Rokubi sensors here
-#endif
-            }
-
-            // Elmo
-            else if(configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Elmo)
-            {
-#ifdef _ELMO_FOUND_
-                std::shared_ptr<elmo::Elmo> elmo_slave_ptr = std::dynamic_pointer_cast<elmo::Elmo>(slave);
-                if(!elmoEnabledAfterStartup)
-                    // Set elmos to operation enabled state, do not block the call!
-                    elmo_slave_ptr->setDriveStateViaPdo(elmo::DriveState::OperationEnabled, false);
-                // set commands if we can
-                if(elmo_slave_ptr->lastPdoStateChangeSuccessful() && elmo_slave_ptr->getReading().getDriveState() == elmo::DriveState::OperationEnabled)
-                {
-                    elmo::Command command;
-                    command.setTargetVelocity(50);
-                    elmo_slave_ptr->stageCommand(command);
-                }
-                else
-                {
-                    MELO_WARN_STREAM("Elmo '" << elmo_slave_ptr->getName() << "': " << elmo_slave_ptr->getReading().getDriveState());
-                    //elmo_slave_ptr->setDriveStateViaPdo(elmo::DriveState::OperationEnabled, false);
-                }
-                auto reading = elmo_slave_ptr->getReading();
-                // std::cout << "Elmo '" << elmo_slave_ptr->getName() << "': "
-                //                 << "velocity: " << reading.getActualVelocity() << " rad/s\n";
-#endif
-            }
-            // Maxon
-            else if (configurator->getInfoForSlave(slave).type == EthercatDeviceConfigurator::EthercatSlaveType::Maxon)
-            {
-#ifdef _MAXON_FOUND_
-
-                // Keep constant update rate
-                // auto start_time = std::chrono::steady_clock::now();
-
-                std::shared_ptr<maxon::Maxon> maxon_slave_ptr = std::dynamic_pointer_cast<maxon::Maxon>(slave);
-
-                if (!maxonEnabledAfterStartup)
-                {
-                    // Set maxons to operation enabled state, do not block the call!
-                    maxon_slave_ptr->setDriveStateViaPdo(maxon::DriveState::OperationEnabled, false);
-                }
-
-                // set commands if we can
-                if (maxon_slave_ptr->lastPdoStateChangeSuccessful() &&
-                        maxon_slave_ptr->getReading().getDriveState() == maxon::DriveState::OperationEnabled)
-                {
-                    maxon::Command command;
-                    command.setModeOfOperation(maxon::ModeOfOperationEnum::CyclicSynchronousTorqueMode);
-                    auto reading = maxon_slave_ptr->getReading();
-                    command.setTargetPosition(reading.getActualPosition() + 10);
-                    command.setTargetTorque(-0.5);
-                    maxon_slave_ptr->stageCommand(command);
-                }
-                else
-                {
-                    MELO_WARN_STREAM("Maxon '" << maxon_slave_ptr->getName()
-                                                                         << "': " << maxon_slave_ptr->getReading().getDriveState());
-                }
-
-                // Constant update rate
-                // std::this_thread::sleep_until(start_time + std::chrono::milliseconds(1));
-
-#endif
-            }
-        }
-        counter++;
-#ifdef _ELMO_FOUND_
-        elmoEnabledAfterStartup = true;
-#endif
-#ifdef _MAXON_FOUND_
-        maxonEnabledAfterStartup = true;
-#endif
+        configurator->getMasters().at(masterIndex)->update(ecat_master::UpdateMode::StandaloneEnforceStep);
+        // for(const auto & master: configurator->getMasters() )
+        // {
+        //     master->update(ecat_master::UpdateMode::StandaloneEnforceStep); // TODO fix the rate compensation (Elmo reliability problem)!!
+        // }
     }
 }
 
@@ -217,28 +108,11 @@ void signal_handler(int sig)
     ** or simliar safety measures at this point using e.g. atomic variables and checking them
     ** in the communication update loop.
      */
-    for(const auto & master: configurator->getMasters())
-    {
-        master->preShutdown();
-    }
 
-    // stop the PDO communication at the next update of the communication loop
-    abrt = true;
-    worker_thread->join();
-
-    /*
-    ** Completely halt the EtherCAT communication.
-    ** No online communication is possible afterwards, including SDOs.
-     */
-    for(const auto & master: configurator->getMasters())
-    {
-        master->shutdown();
-    }
+    abrtMain = true;
 
     // Exit this executable
     std::cout << "Shutdown" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    exit(0);
 }
 
 #ifdef _ANYDRIVE_FOUND_
@@ -310,8 +184,14 @@ int main(int argc, char**argv)
         }
     }
 
+    // worker_thread = std::make_unique<std::thread>(&worker);
     // Start the PDO loop in a new thread.
-    worker_thread = std::make_unique<std::thread>(&worker);
+    std::vector<std::thread> threads;
+    for (unsigned int i = 0; i < configurator->getMasters().size(); i++) {
+        std::thread t(worker,i);
+        t.detach();
+        // threads.push_back(t);
+    }
 
     /*
     ** Wait for a few PDO cycles to pass.
@@ -332,9 +212,30 @@ int main(int argc, char**argv)
 #endif
     }
 
-
     std::cout << "Startup finished" << std::endl;
 
-    // nothing further to do in this thread.
-    pause();
+    while (true) {
+        if (abrtMain) {
+            for(const auto & master: configurator->getMasters())
+            {
+                master->preShutdown();
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            abrt = true;
+            for ( unsigned int i = 0; i < threads.size(); i++) {
+                threads.at(i).join();
+            }
+            // worker_thread->join();
+            for(const auto & master: configurator->getMasters())
+            {
+                master->shutdown();
+            }
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    std::cout << "Exit" << std::endl;
+    exit(0);
+
 }
